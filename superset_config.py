367ea34c8d2c1e8d525df5467ba249ff06c07b5e
Andrey Mikhailov <andrey.mikhailov@valamis.com>
2021-12-13 00:23:01 +0300

    feat: use postgres instead of sqlite

--- superset_config.py
+++ superset_config.py
@@ -1,6 +1,7 @@
 ENABLE_PROXY_FIX = True
 
-# SQLALCHEMY_DATABASE_URI = 'sqlite:////app/superset_home/superset.db'
+SQLALCHEMY_DATABASE_URI = \
+    'postgresql+psycopg2://superset:superset@postgres:5432/superset'
 
 from reverse_proxied import ReverseProxied
 ADDITIONAL_MIDDLEWARE = [ReverseProxied, ]
